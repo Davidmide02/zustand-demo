c5c32d8d2b8e95671edf4e33b22f111993d6c4aa
Davidmide <Davidmide07@gmail.com>
2024-05-27 14:18:58 +0100

    remove logs

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -3,20 +3,12 @@ import { create } from "zustand";
 export const useCartStore = create((set) => ({
   cart: [], //initial value
 
-  addToCart: (product) => {
-    set((state) => ({ cart: [...state.cart, product] }));
-    console.log("add button clicked");
-  },
+  addToCart: (product) => set((state) => ({ cart: [...state.cart, product] })),
 
-  removeFromCart: (productId) => {
+  removeFromCart: (productId) =>
     set((state) => ({
       cart: state.cart.filter((product) => product.id !== productId),
-    }));
-    console.log("clik remmbied");
-  },
+    })),
 
-  clearCart: () => {
-    set({ cart: [] });
-    console.log("clear cart");
-  },
+  clearCart: () => set({ cart: [] }),
 }));
